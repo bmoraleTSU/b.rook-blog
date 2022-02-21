f061db83412dc42809fbc88a35050d347c260b44
bmoraleTSU <100156666+bmoraleTSU@users.noreply.github.com>
2022-02-21 12:57:07 -0600

    Changed names to custom names
    Changed global name variable and blog name to custom names

---
+++ global-data.js
@@ -0,0 +1,18 @@
+export const getGlobalData = () => {
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
+    : 'Brooke Jaelyn';
+  const blogTitle = process.env.BLOG_TITLE
+    ? decodeURI(process.env.BLOG_TITLE)
+    : 'My Blog';
+    
+  const footerText = process.env.BLOG_FOOTER_TEXT
+    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+    : 'All rights reserved.';
+
+  return {
+    name,
+    blogTitle,
+    footerText,
+  };
+};
